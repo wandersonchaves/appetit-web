6577670db4c10e0e177893eae0c77c2df8aa79a9
WandersonChaves <wandersonscpibr@gmail.com>
2020-06-13 11:35:42 -0300

    add authentication

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,27 @@
 import React from "react";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
+import { isAutenticated } from "./auth";
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={(props) =>
+      isAutenticated() ? (
+        <Component {...props} />
+      ) : (
+        <Redirect to={{ pathname: "/", state: { from: props.location } }} />
+      )
+    }
+  />
+);
+
 const Routes = () => (
   // eslint-disable-next-line no-unused-expressions
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={() => <h1>Hello World</h1>} />
+      <PrivateRoute path="/app" component={() => <h1>Você está logado</h1>} />
     </Switch>
   </BrowserRouter>
 );
