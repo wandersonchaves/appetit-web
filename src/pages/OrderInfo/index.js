94c9b326d65c7b03e0178f08f4246ff2c1c297c4
WandersonChaves <wandersonscpibr@gmail.com>
2020-06-19 17:04:26 -0300

    add componente para lista de produtos do pedido

--- src/pages/OrderInfo/index.js
+++ src/pages/OrderInfo/index.js
@@ -18,8 +18,7 @@ import CheckCircleOutlinedIcon from "@material-ui/icons/CheckCircleOutlined";
 import PageTitle from "../../components/PageTItle";
 import avatar1 from "../../assets/avatarProducts/avatar1.png";
 import { AppContext, OrderContext } from "../../context";
-import { Button } from "@material-ui/core";
-import ChevronRightOutlinedIcon from "@material-ui/icons/ChevronRightOutlined";
+import ProductsList from "./ProductsList";
 
 const BorderLinearProgress = withStyles((theme) => ({
   root: {
@@ -36,71 +35,11 @@ const BorderLinearProgress = withStyles((theme) => ({
   },
 }))(LinearProgress);
 
-const ProductListItem = withStyles((theme) => ({
-  root: {
-    borderRadius: 0,
-    backgroundColor: "transparent",
-    boxShadow: "none",
-    borderBottom: "1px solid #ddd",
-  },
-}))(ListItem);
-
-// TODO criar component compartilhado
-const NextButton = withStyles((theme) => ({
-  root: {
-    borderRadius: 5,
-  },
-}))(Button);
-
-// retorna os produtos agrupados por categoria para facilitar a criacao da lista de produtos no pedido
-/*
-[{name: 'category', products: []}]
-*/
-const getProductsByCategory = (products) => {
-  const result = [];
-  const findGroup = (name) => result.find((group) => group.name === name);
-
-  products.forEach((product) => {
-    const group = findGroup(product.category);
-    if (group) {
-      group.products.push(product);
-    } else {
-      result.push({ name: product.category, products: [product] });
-    }
-  });
-  return result;
-};
-
-// retorna um object com os produtos tendo o id como chave
-const getProductsById = (products) => {
-  const result = {};
-  products.forEach((product) => {
-    result[product.id] = product;
-  });
-  return result;
-};
-
 // TODO criar arquivo OrderInfo/index.js
 export default function OrderInfo() {
   const theme = useTheme();
   const history = useHistory();
-  const {
-    state: { products },
-  } = React.useContext(AppContext);
-  const { state: order, dispatch } = React.useContext(OrderContext);
-  const productsByCategory = getProductsByCategory(products);
-  const productsById = getProductsById(products);
-
-  // soma da quantidade e preco de cada produto do pedido
-  const getTotal = () => {
-    let total = 0;
-    Object.keys(order.products).forEach((id) => {
-      total += order.products[id].quantity * productsById[id].price;
-    });
-    return total;
-  };
-
-  const hasProducts = () => Object.keys(order.products).length > 0;
+  const { state: order } = React.useContext(OrderContext);
 
   const progress = Math.ceil(order.step * 33.3);
 
@@ -134,85 +73,7 @@ export default function OrderInfo() {
           }}
         />
       </Box>
-      <Box mx={5} mt={2}>
-        {productsByCategory.map((category, i) => (
-          <List
-            dense
-            key={i}
-            subheader={
-              <ListSubheader component="div" id="nested-list-subheader">
-                <Typography>{category.name}</Typography>
-              </ListSubheader>
-            }
-          >
-            {category.products.map((product) => {
-              const labelId = `checkbox-list-secondary-label-${product.id}`;
-
-              return (
-                <ProductListItem
-                  color="#000"
-                  key={product.id}
-                  button
-                  onClick={() => history.push(`/products/${product.id}`)}
-                >
-                  <ListItemAvatar>
-                    {order.products[product.id] ? (
-                      <Avatar>
-                        <CheckCircleOutlinedIcon />
-                      </Avatar>
-                    ) : (
-                      <Avatar alt={product.name} src={product.avatar} />
-                    )}
-                  </ListItemAvatar>
-                  <ListItemText id={labelId} primary={product.name} />
-                  <ListItemSecondaryAction>
-                    <Typography variant="body2">{product.price}</Typography>
-                  </ListItemSecondaryAction>
-                </ProductListItem>
-              );
-            })}
-          </List>
-        ))}
-      </Box>
-      {hasProducts() && (
-        <Box
-          display="flex"
-          alignItems="center"
-          justifyContent="space-between"
-          bgcolor={theme.palette.primary.main}
-        >
-          <Box mx={3} my={2} color="#fff">
-            <b>Total: R$ {getTotal()}</b>
-          </Box>
-          <Box m={2}>
-            <NextButton
-              style={{ color: "#fff" }}
-              endIcon={<ChevronRightOutlinedIcon />}
-              onClick={() => dispatch({ type: "next_step" })}
-            >
-              Avançar
-            </NextButton>
-          </Box>
-        </Box>
-      )}
+      <ProductsList />
     </>
   );
 }
-
-/*
-
-[
-  {
-    name: 'Cuzcuz'
-    products: [
-      {id: 'p1', name: 'Cuzcuz simples', category: 'Cuzcuz', price: 3.25}
-    ]
-  },
-  {
-    name: 'Pães',
-    products: [
-
-    ]
-  }
-]
- */
